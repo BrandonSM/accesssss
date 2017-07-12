2a6dec7074b3c77ba155fb13e54bb2d39b92cf78
Brandon M <development@brandonmcginnis.com>
2017-07-11 21:52:54 -0400

    Added styling to Header and installed firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Button, Card, CardSection, Header } from './';
+import { Header } from './';
+
 
 class App extends Component {
 	render() {
 		return(
 			<View>
+				<Header headerText="Authentication" />
 				<Text>Accessss</Text>
 			</View>
 		);
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,10 +13,19 @@ const Header = (props) => {
 
 const styles = StyleSheet.create({
 	viewStyle: {
-
+		backgroundColor:'#F8F8F8',
+		justifyContent:'center',
+		alignItems:'center',
+		height: 65,
+		paddingTop:20,
+		shadowColor:'#000001',
+		shadowOffset: { width: 0, height: 1 },
+		shadowOpacity: 0.2,
+		elevation: 2,
+		position: 'relative',
 	},
 	textStyle: {
-
+		fontSize: 20,
 	},
 });
 
