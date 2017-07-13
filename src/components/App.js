f16135db32ad4fb8bc9fad8ec3a6e2186d2c78a6
Brandon M <development@brandonmcginnis.com>
2017-07-12 20:23:06 -0400

    Added firebase and config to the project

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './';
-
+import firebase from 'firebase';
+const config = require('./../../config/firebase.config.js');
 
 class App extends Component {
+
+	componentWillMount() {
+		firebase.initializeApp(config);
+	}
+
 	render() {
 		return(
 			<View>
