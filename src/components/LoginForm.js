b8a1de0f85bd7b2cc542ffe93e1aa6a409e49cc9
Brandon M <development@brandonmcginnis.com>
2017-07-24 19:22:15 -0400

    Added the TextInput on the LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { TextInput, View } from 'react-native';
 import { Button, Card, CardSection } from './';
 
 class LoginForm extends Component {
 	render() {
 		return(
 			<Card>
-				<CardSection/>
+				<CardSection>
+					<TextInput style={styles.textInputStyle}></TextInput>
+				</CardSection>
 				<CardSection/>
 				<CardSection>
-					<Button>LOGIN</Button>
+					<Button style={styles.buttonStyle}>LOGIN</Button>
 				</CardSection>
 			</Card>
 		);
 	}
 }
+
+const styles = {
+	textStyle: {
+		alignSelf: 'center',
+		color: '#007aff',
+		fontSize: 16,
+		fontWeight: '600',
+		paddingTop: 10,
+		paddingBottom: 10
+	},
+	textInputStyle: {
+		width:100,
+		height:25,
+		borderWidth:1,
+		borderColor:'#AFAFAF',
+	},
+	buttonStyle: {
+		flex: 1,
+		alignSelf: 'stretch', // Stretch to fill limits of container
+		backgroundColor: '#fff',
+		borderRadius: 5,
+		borderWidth: 1,
+		borderColor: '#007aff',
+		marginLeft: 5,
+		marginRight: 5,
+	}
+};
 
 export { LoginForm };
