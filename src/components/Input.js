80b3936d3aecda5363b641c8f0b7fdf9b515eff6
Brandon M <development@brandonmcginnis.com>
2017-08-01 22:36:46 -0400

    Added styling to Input

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,14 +4,37 @@ import { Text, TextInput, View } from 'react-native';
 const Input = ({ label, value, onChangeText }) => {
 	return (
 		<View>
-			<Text>{label}</Text>
+			<Text style={styles.labelStyle}>{label}</Text>
 			<TextInput
 				value={value}
 				onChangeText={onChangeText}
-				style={{ height:20, width:100 }}
+				style={styles.inputStyle}
 			/>
 		</View>
 	);
+};
+
+const styles = {
+	inputStyle: {
+		color: '#000',
+		paddingRight: 5,
+		paddingLeft: 5,
+		fontSize: 18,
+		lineHeight: 23,
+		flex:2,
+	},
+	labelStyle: {
+		fontSize: 18,
+		paddingLeft: 20,
+		flex: 1,
+	},
+	constainerStyle: {
+		flexDirection: 'row',
+		flex: 1,
+		height: 40,
+		align: 'center',
+	},
+
 };
 
 export { Input };
